5258ba0bd29ad395e8ce3d1d0ca82430b2585e45
Israel Fonseca <israel@meuspedidos.com.br>
2016-10-26 21:07:44 -0200

    first (and huge) commit

---
+++ ConceptTest.js
@@ -0,0 +1,78 @@
+assert = require('chai').assert
+
+class PromiseStop extends Error {
+}
+
+const FOUND_CONFIG = 0
+const NO_USER = 1
+
+function fetchConfigForUserUsecaseWithException(userName, fetchUsersGateway, fetchUserConfigGateway) {
+    return new Promise((resolve, reject) => {
+        fetchUsersGateway(userName)
+            .then((users) => {
+                if (users.length >= 1) {
+                    return fetchUserConfigGateway(users[0])
+                } else {
+                    resolve({action: NO_USER, data: 'No users. :('}) // this is the output to the user, implemented apart from the app core
+                    throw new PromiseStop() // You can resolve your promise and still raise an Error to break the chain...
+                }
+            })
+            .then((config) => {
+                resolve({action: FOUND_CONFIG, data: config}) // this then() won't be executed in case of failure
+            })
+            .catch((error) => {
+                if (!(error instanceof PromiseStop)) { // ...but we still have to ignore our custom error, and redirect the real ones.
+                    reject(error)
+                }
+            })
+    })
+}
+
+function fetchConfigForUserUsecaseWithNestedPromise(userName, fetchUsersGateway, fetchUserConfigGateway) {
+    return new Promise((resolve, reject) => {
+        fetchUsersGateway(userName)
+            .then((users) => {
+                if (users.length >= 1) {
+                    return fetchUserConfigGateway(users[0])
+                        .then((config) => { // Nested promises are ugly, imagine that you have more cases in here.
+                            resolve({action: FOUND_CONFIG, data: config})
+                        })
+                } else {
+                    resolve({action: NO_USER, data: 'No users. :('})
+                    throw new PromiseStop()
+                }
+            })
+            .catch((error) => {
+                reject(error)
+            })
+    })
+}
+
+describe('Clean Architecture with Promises concept', () => {
+
+    function fetchUsersGatewayFake(name) {
+        return Promise.resolve(['user1', 'user2'].filter((user) => user == name))
+    }
+
+    function fetchUserConfigGatewayFake(user) {
+        return Promise.resolve('nice info for user: ' + user)
+    }
+
+
+    let usecase = fetchConfigForUserUsecaseWithException
+    // let usecase = fetchConfigForUserUsecaseWithNestedPromise
+
+    it('should fetch user config (the happy path)', () => {
+        return usecase('user1', fetchUsersGatewayFake, fetchUserConfigGatewayFake).then(result => {
+            assert.equal(result.action, FOUND_CONFIG)
+            assert.equal(result.data, 'nice info for user: user1')
+        })
+    })
+
+    it('should present a failure', () => {
+        return usecase('invalid-user', fetchUsersGatewayFake, fetchUserConfigGatewayFake).then(result => {
+            assert.equal(result.action, NO_USER)
+            assert.equal(result.data, 'No users. :(')
+        })
+    })
+})
---
+++ README.md
@@ -0,0 +1,24 @@
+# Clean Architecture with Promises + Node.js (version 6+)
+
+This sample project, shows a proposal of how implement the Clean Architecture with Node.js, ES6 Promises and simple functions.
+ 
+![](https://8thlight.com/blog/assets/posts/2012-08-13-the-clean-architecture/CleanArchitecture-8b00a9d7e2543fa9ca76b81b05066629.jpg)
+ 
+- **Entity:** Any pure function shared across the usecases.
+- **Usecase:** Just a function with arguments. Gateways are inject as simple function arguments (`Currying` can help to avoid some verbosity). It should return a Promise because the gateways are probably going to be async too.
+- **Gateways:** Non-pure functions that are passed as arguments to the usecase.
+- **Presenter:** Implemented as the `resolve` method of the usecase Promise. We can pass an object to it with the action name and the data related to the event.
+- **Controller:** Anything that will invoke the Usecase (Browser or Node)
+
+But, there's a gotcha: Breaking promises chain is not quite straightforward. So far, it seems that the only way (without ES7 or external libraries) is just throwing a custom exception. As alternative I could just chain the promises, but that would be the infamous "callback hell" with a new syntax (Promises exist to solve this problem too, right?).
+
+Any ideas? Make a PR and let discuss. :)
+
+
+## Test
+
+`npm run test`
+
+You can swap the usecase implementation of the test easily in the source code, play with it!
+
+**Rembember! you need to use the latest Node version that support ES6 syntax**
